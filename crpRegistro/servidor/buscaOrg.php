68a5fcba23e39d0f8ebed3d0271ce01eed644611
chrystianruan <80183113+chrystianruan@users.noreply.github.com>
2021-12-30 12:55:49 -0300

    Add files via upload

---
+++ crpRegistro/servidor/buscaOrg.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+
+    if((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true) or ($_SESSION['nivelAcesso'] != 30))
+    {
+        unset($_SESSION['usuario']);
+        unset($_SESSION['senha']);
+        header('Location: /crpRegistro/index.html');
+        session_destroy();
+    }
+  
+    $logado = $_SESSION['usuario'];
+    
+?>
+
+<?php
+function getFilterDemonstrativo($objeto){
+	$pdo = new PDO("mysql:host=127.0.0.1; dbname=crp;", "compras", "#compr@s20");
+	$sql = 'SELECT *
+FROM orgao as o
+WHERE EXISTS
+(SELECT *
+FROM registro as r
+WHERE o.id = r.id_orgao AND id_objeto = ? HAVING qtdDemandaOrg) ORDER BY nome;';
+	$stm = $pdo->prepare($sql);
+	$stm->bindValue(1, $objeto);
+	$stm->execute();
+	
+	echo json_encode($stm->fetchAll(PDO::FETCH_ASSOC));
+	$pdo = null;
+}
+
+if(isset($_POST['objeto'])){
+	$objeto = $_POST['objeto'];
+	getFilterDemonstrativo($objeto);	
+}
+
+?>
